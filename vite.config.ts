00636585d39cb2c7046ce16707998d66267e4933
Caio Rodrigues <caiorodrigues159@gmail.com>
2025-01-03 19:02:28 -0300

    ajustes 5

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['emailjs-com'], // Exclui emailjs-com do bundle
-    },
-  },
-  base: '/portifolio-caio/', // Substitua com o nome correto do seu repositório
+  base: '/portifolio-caio/',  // Corrija para o nome do seu repositório
 });
